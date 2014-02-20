18f0bc7f0ce77e159d2215e1ef643414ddf4ebb4
Yuki MIZUNO <mizuyuu0904@gmail.com>
2014-02-20 11:27:23 +0900

    version 0.0.1

---
+++ README.md
@@ -0,0 +1,8 @@
+Unicode Encode
+===
+[__PopClip Extention__](https://pilotmoon.com/popclip/)
+
+
+Just Unicode encodes (c.f. \u006b) the selected text using bash.
+
+*Supportt only UTF-8 characters.*
---
+++ unicodeencode.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#support UTF-8 characters
+export LANG="en_US.UTF-8"
+
+for ((i = 0; i < ${#POPCLIP_TEXT}; i++)) {
+
+    char=${POPCLIP_TEXT:$i:1}
+
+    utf8_raw="0x$(xxd -ps <<< $char)"
+    digit_raw=$(printf "%d" $utf8_raw)
+
+    #remove '0a'
+    digit=$(expr $digit_raw / 256)
+
+    if [ $digit -lt 256 ]; then
+        unicode=$digit
+    elif [ $digit -lt 65536 ]; then
+        unicode=$(expr \( $digit / 256 - 192 \) \* 64 + \
+            $digit % 256 - 128 )
+    elif [ $digit -lt 16777216 ]; then
+        unicode=$(expr \( $digit / 65536 - 224 \) \* 4096 + \
+            \( \( $digit / 256 \) % 256 - 128 \) \* 64 + \
+            $digit % 256 - 128 )
+    else
+        unicode=$(expr \( $digit / 16777216 - 240 \) \* 262144 + \
+            \( \( $digit / 65536 \) % 256 - 128 \) \* 4096 + \
+            \( \( $digit / 256 \) % 256 - 128 \) \* 64 + \
+            $digit % 256 - 128 )
+    fi
+
+    printf "\\u%04x" $unicode
+
+}
